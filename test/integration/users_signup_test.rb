a9c434e3cf3d688c446bdd821a6450e6ea4e1251
Tiffany Wei <tiffanywei90@gmail.com>
2015-04-02 21:49:02 +0000

    Finish user signup, including invalid field error pages, as well as end to end new user sign up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Good Name",
+                                    email: "user@valid.com",
+                                    password:              "foobar",
+                                    password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
+  end
+end

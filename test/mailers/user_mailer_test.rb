637107a1e939b6b065ed8f1e3a61821121711069
Tiffany Wei <tiffanywei90@gmail.com>
2015-04-04 18:02:21 +0000

    Add password resets and email confirmations

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,7 +6,7 @@ class UserMailerTest < ActionMailer::TestCase
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["michael@example.com"], mail.to
+    assert_equal [user.email], mail.to
     assert_equal ["from@example.com"], mail.from
     assert_match user.name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
@@ -14,11 +14,14 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
-    mail = UserMailer.password_reset
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal [user.email], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
   end
 
 end
